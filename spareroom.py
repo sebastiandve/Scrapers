7c61740afb3da503a751902fd499b02b4747f034
Sebastian <sebastian.delgado@edgepetrol.com>
2020-05-05 09:10:19 +0100

    tweaks

--- spareroom.py
+++ spareroom.py
@@ -9,12 +9,20 @@ def get_contact_status(item):
     except:
         return "Not contacted"
 
+def is_contactable(item):
+    try:
+        item.find('footer').find('span', {'class': 'freeContact status'}).get_text().strip()
+    except:
+        return "Early Bird"
+
 message = """Hey,
 
-I'm looking for someone to replace me in my current house in Maida Vale ASAP.
-This is my ad: www.spareroom.co.uk/flatshare/ london/maida_vale/14904769
+I'm looking for someone to replace me in my current house in Maida Vale and can move in June at the latest.
+This is my ad: www.spareroom.co.uk/flatshare/london/maida_vale/14904769
 
 Please let me know if you have any questions.
+
+PS: A second room in the house is also available
 
 Cheers
 Sebastian
@@ -32,12 +40,12 @@ driver.find_element_by_id("loginpass").send_keys("5RM&MBzawbs5")
 driver.find_element_by_id("sign-in-button").click()
 
 
-url = "https://www.spareroom.co.uk/flatmate/flatmates.pl?flatshare_type=wanted&=&search_id=956693413&offset={}&sort_by=days_since_placed"
+url = "https://www.spareroom.co.uk/flatmate/flatmates.pl?flatshare_type=wanted&=&search_id=959113513&offset={}&sort_by=days_since_placed"
 
 
 listings = []
 
-for page in range(0, 651, 10):
+for page in range(0, 991, 10):
     time.sleep(1)
     print(page, len(listings))
     driver.get(url.format(page))
@@ -48,26 +56,32 @@ for page in range(0, 651, 10):
         current = {}
         current['title'] = result.find('header').find('a').find('h1').get_text().strip()
         current['url'] = result.find('header').find('a')['href']
-        # current['status'] = result.find('footer').find('span').get_text().strip()
+        current['contactable'] = is_contactable(result)
         current['status'] = get_contact_status(result)
         listings.append(current)
 
 df = pd.DataFrame(listings)
+print(len(listings))
+print(df.status.value_counts())
 
 count=0
 for i, l in enumerate(listings):
     if 'couple' in l['title'].lower():
         count+=1
         continue
-    if l['status'].strip() == 'Contacted':
+    elif l['status'].strip() == 'Contacted':
         count += 1
         continue
-    print(count, l['title'], l['status'])
+    elif l['contactable'] == 'Early Bird':
+        count += 1
+        continue
+    else:
+        print(count, l['title'], l['status'])
 
-    driver.get("https://www.spareroom.co.uk"+l['url']+'&mode=contact&submode=byemail')
-    driver.find_element_by_id('message').send_keys(message)
-    driver.find_element_by_class_name('contact-form__submit').click()
-    count += 1
-    time.sleep(3)
+        driver.get("https://www.spareroom.co.uk"+l['url']+'&mode=contact&submode=byemail')
+        driver.find_element_by_id('message').send_keys(message)
+        driver.find_element_by_class_name('contact-form__submit').click()
+        count += 1
+        time.sleep(1)
 
 
